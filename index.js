a40a0c4cad7419e74abbefac409764f1b4b0de60
lesi <lesitatisel@gmail.com>
2025-05-16 18:26:27 +0500

    Get button input and display if it's a digit.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let firstNumber, secondNumber, operator;
+let displayValue = '';
+
+const display = document.querySelector(".display");
+const btn = document.querySelectorAll(".btn");
 
 const add = function (a, b) {
   return a + b;
@@ -23,3 +27,24 @@ const divide = function (a, b) {
 const operate = function (operation, firstNumber, secondNumber) {
   return operation(firstNumber, secondNumber);
 }
+
+
+// Create the functions that populate the display when you click the digit buttons. You should store the content of the display (the number) in a variable for use in the next step.
+const nonDigits = ['+', '-', '*', '/', '=', 'C', 'P', '⌫'];
+const getBtnValue = function () {
+  btn.forEach( btn => {
+    btn.addEventListener('click', (e) => {
+      const btnVal = e.target.textContent.trim();
+      // console.log(e.target.textContent.trim());
+      if (!nonDigits.includes(btnVal)) {
+        display.textContent += btnVal;
+      }
+    })
+  })
+}
+  
+const populateDisplay = function () {
+  getBtnValue();
+}
+
+populateDisplay();

4bd044656d1feecdcecdf8a04b3e694bb01562a7
lesi <lesitatisel@gmail.com>
2025-05-16 19:38:17 +0500

    Update calculator logic

--- index.js
+++ index.js
@@ -1,50 +1,57 @@
-let firstNumber, secondNumber, operator;
-let displayValue = '';
+let firstNumber;
+let secondNumber;
+let operator;
+let displayValue;
+
+const nonDigits = ['+', '-', '*', '/', '=', 'C', 'P', '⌫']
 
 const display = document.querySelector(".display");
-const btn = document.querySelectorAll(".btn");
+const buttons = document.querySelectorAll(".btn");
+const clearBtn = document.querySelector(".clear-btn")
 
-const add = function (a, b) {
-  return a + b;
-}
+const add = (a, b) => { a + b };
 
-const subtract = function (a, b) {
-  return a - b;
-}
+const subtract = (a, b) => { a - b };
 
-const multiply = function (a, b) {
-  return a * b;
-}
+const multiply = (a, b) => { a + b };
 
-const divide = function (a, b) {
-  if ( b == 0 ) {
-    console.log("Cannot divide by zero.");
-    return false;
+const divide = (a, b) => { b !== 0 ? a / b : display.textContent = "Cannot divide by zero." }
+
+const operate = (operation, firstNumber, secondNumber) => { operation(firstNumber, secondNumber)};
+
+const clear = () => { display.textContent = "" };
+
+const getOperator = (op) => {
+  switch (op) {
+    case '+':
+      operator = add;
+      break;      
+    case '-':
+      operator = subtract;
+      break;
+    case '*':
+      operator = multiply;
+      break;
+    case '/':
+      operator = divide;
+      break;
+    case 'C':
+      clear();
+      break;
   }
-  return a / b;
 }
 
-const operate = function (operation, firstNumber, secondNumber) {
-  return operation(firstNumber, secondNumber);
-}
+// Create the functions that populate the display when you click the digit buttons. You should store the content of the display (the number) in a variable for use in 
 
-
-// Create the functions that populate the display when you click the digit buttons. You should store the content of the display (the number) in a variable for use in the next step.
-const nonDigits = ['+', '-', '*', '/', '=', 'C', 'P', '⌫'];
-const getBtnValue = function () {
-  btn.forEach( btn => {
-    btn.addEventListener('click', (e) => {
-      const btnVal = e.target.textContent.trim();
-      // console.log(e.target.textContent.trim());
-      if (!nonDigits.includes(btnVal)) {
-        display.textContent += btnVal;
-      }
-    })
-  })
-}
-  
-const populateDisplay = function () {
-  getBtnValue();
-}
-
-populateDisplay();
+buttons.forEach( (btn) => {
+  btn.addEventListener('click', (e) => {
+    const buttonValue = e.target.textContent.trim();
+    if (!nonDigits.includes(buttonValue)) {
+      display.textContent += buttonValue;
+      displayValue = Number(display.textContent);
+    } else{
+      getOperator(buttonValue);
+      clear();
+    }
+  });
+});
